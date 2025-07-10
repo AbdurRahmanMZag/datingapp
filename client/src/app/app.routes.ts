5305769eb2790394521ac093f46d0f8678f3d06c
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2025-07-10 17:55:28 +0300

    End of section 6

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
+import { MemberList } from '../features/members/member-list/member-list';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { Home } from '../features/home/home';
+import { authGuard } from '../core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    {
+        path: '',
+        canActivate: [authGuard],
+        runGuardsAndResolvers: 'always',
+        children: [
+            { path: 'lists', component: Lists },
+            { path: 'messages', component: Messages },
+            { path: 'members', component: MemberList },
+            { path: 'members/:id', component: MemberDetailed },
+        ]
+    },
+    { path: '**', component: Home } // Wildcard route for a 404 page or redirect to home
+];
--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -1,28 +1,34 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../core/services/account-service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../core/services/toast-service';
 
 @Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.html',
   styleUrl: './nav.css'
 })
 export class Nav {
-  protected accountService = inject(AccountService)
-  protected creds: any = {}
+  protected toastService = inject(ToastService);
+  protected router = inject(Router);
+  protected accountService = inject(AccountService);
+  protected creds: any = {};
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
-        console.log(result);
+        this.toastService.success('Login successful');
+        this.router.navigateByUrl('/members');
         this.creds = {};
       },
-      error: error => alert(error.message)
+      error: error => this.toastService.error(error.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
