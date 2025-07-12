aded01711b20f19b55994175df78dac1abb60231
AbdurRahamn M Zaghloul <drabdohza@gmail.com>
2025-07-12 12:02:16 +0300

    Error handling

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { ServerError } from './../shared/errors/server-error/server-error';
 import { Routes } from '@angular/router';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
@@ -5,6 +6,8 @@ import { MemberList } from '../features/members/member-list/member-list';
 import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
 import { Home } from '../features/home/home';
 import { authGuard } from '../core/guards/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
 
 export const routes: Routes = [
     { path: '', component: Home },
@@ -19,5 +22,8 @@ export const routes: Routes = [
             { path: 'members/:id', component: MemberDetailed },
         ]
     },
-    { path: '**', component: Home } // Wildcard route for a 404 page or redirect to home
+    { path: 'errors', component: TestErrors },
+    { path: 'not-found', component: NotFound },
+    { path: 'server-error', component: ServerError },
+    { path: '**', component: NotFound } // Wildcard route for a 404 page or redirect to home
 ];
